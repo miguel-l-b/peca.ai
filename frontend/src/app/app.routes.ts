d7c4b4916b4d5bb572882300c63753b5acd2ea98
Miguel Lopes Braido <miguellopesbraido@gmail.com>
2024-08-31 17:44:48 -0300

    chore: configure default images, HTML templates, and routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { VehicleComponent } from './pages/vehicle/vehicle.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+import { PartsComponent } from './pages/parts/parts.component';
+import { PartComponent } from './pages/part/part.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: 'vehicle/:id', component: VehicleComponent },
+  { path: 'parts', component: PartsComponent },
+  { path: 'part/:id', component: PartComponent },
+  { path: '**', component: NotFoundComponent }
+];
